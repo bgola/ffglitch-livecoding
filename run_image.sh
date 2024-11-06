74f89445d7183b660c51db67035ea8f0577fe84d
Bruno Gola <me@bgo.la>
2024-11-06 11:28:35 +0100

    add helper scripts

---
+++ run_image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMG=$1
+
+. .env/bin/activate
+
+python zmqserver_livecoding_watchdog.py template.js & 
+PID=$!
+
+./bin/ffgac -loop 1 -i "${IMG}" -vcodec mpeg4 -mpv_flags +nopimb+forcemv -qscale:v 1 -t 10050 -fcode 5 -g max -sc_threshold max -mb_type_script scripts/mb_type_func_live.js -f rawvideo pipe: | ./bin/fflive -i pipe: -s scripts/livecoding.js
+
+kill -9 $PID
